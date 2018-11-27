455b778aebad0f5f536c878d16354e5dae3d34df
John Korchemniy <johnkorchemniy@gmail.com>
2018-11-26 18:28:40 -0800

    Added api key for others to use

--- sta-service.js
+++ sta-service.js
@@ -1,5 +1,8 @@
 (function (services) {
 
+    // Google API key... Should be good for map, transit, and places apis
+    // AIzaSyC6Bmqdj6b6GsrGyOsawtQscPwcGgzBF8g
+
     services.sta = new STAService();
 
     function STAService() {
