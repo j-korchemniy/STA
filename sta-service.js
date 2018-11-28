d457109975956d0d6fb22936d80ff2889dc53209
John Korchemniy <johnkorchemniy@gmail.com>
2018-11-28 11:47:53 -0800

    Backup

--- sta-service.js
+++ sta-service.js
@@ -21,9 +21,49 @@
             var map = new google.maps.Map(document.getElementById('map'), options);
             var transitLayer = new google.maps.TransitLayer();
             transitLayer.setMap(map);
+
+            infowindow = new google.maps.InfoWindow();
+            var service = new google.maps.places.PlacesService(map);
+            service.nearbySearch({
+                location: options.center,
+                radius: 1000,
+                types: ['bus_station', 'transit_station']
+
+            }, callback);
+
+            function callback(results, status) {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    // for (var i = 0; i < results.length; i++) {
+                    //     createMarker(results[i]);
+                    // }
+                    JSON.stringify(results);
+                }
+            }
         }
 
         this.loadServicingRoutes = function () {
+
+            var options = {
+                "location": location.latitude + ',' + location.logitude,
+                "sensor": true,
+                "key": "AIzaSyC6Bmqdj6b6GsrGyOsawtQscPwcGgzBF8g",
+                "rankby": "distance",
+                "types": "bus_station"
+            };
+
+            $.ajax({
+                url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
+                type: "GET",
+                dataType: 'jsonp',
+                data: options,
+                success: function (response) {
+                    console.log(JSON.stringify(response));
+                },
+                error: function (xhr) {
+                    console.error("Failed to get nearby bus stop information.");
+                }
+            });
+
             console.log("Loading servicing buses information. " + JSON.stringify(location));
         }
 
